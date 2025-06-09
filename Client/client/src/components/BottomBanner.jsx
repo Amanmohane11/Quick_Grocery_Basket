1717f1e308f9e4c82e81edb2f2a1452c92b21ca3
Amanmohane11 <164287485+Amanmohane11@users.noreply.github.com>
2025-06-09 11:21:12 -0400

    Update BottomBanner.jsx

--- Client/client/src/components/BottomBanner.jsx
+++ Client/client/src/components/BottomBanner.jsx
@@ -3,19 +3,19 @@ import { assets, features } from '../assets/assets'
 
 const BottomBanner=()=>{
     return (
-        <div className='relative w-full mt-16 p-5'>
-            <img src={assets.bottom_banner_image} alt='banner' className='w-full lg:h-110git commit -m "Your commit message here"git pull origin main --rebase
+        <div className='relative w-full mt-16 p-5 '>
+            <img src={assets.bottom_banner_image} alt='banner' className='w-full lg:h-115 
              hidden md:block'/>
             <img src={assets.bottom_banner_image_sm} alt='banner' className='w-full md:hidden'/>
             <div className='absolute inset-0 flex flex-col items-center md:items-end
             md:justify-center pt-16 md:pt-0 px-6 md:px-24'>
-                    <h1 className='text-2xl md:text-3xl font-semibold text-primary-dull mb-7'>Why We Are The Best?</h1>
-                    <div  className="flex flex-col items-center gap-4 w-full max-w-screen-md md:items-end">
+                    <h1 className='text-2xl md:text-3 font-semibold text-primary-dull mb-7'>Why We Are The Best?</h1>
+                    <div  className="flex flex-col items-center md:text-2xl gap-1 md:gap-1 w-full md:ml-5 max-w-screen-md md:items-end">
                     {features.map((feature,i)=>(
-                        <div key={i} className='flex items-center gap-4 '>
-                            <img src={feature.icon} alt={feature.title} className='md:w-11 w-11'/>
-                            <div >
-                            <h3 className='text-lg md:text-xl font-semibold'>{feature.title}</h3>
+                        <div key={i} className='flex items-center gap-4  '>
+                            <img src={feature.icon} alt={feature.title} className='md:w-9 w-11 '/>
+                            <div className=' md:text-sm lg:text-3xl' >
+                            <h3 className='text-lg md:text-1xl font-semibold'>{feature.title}</h3>
                             <p className='text-gray-500/70 text-xs md:text-sm'>{feature.description}</p>
                             </div>
                         </div>
