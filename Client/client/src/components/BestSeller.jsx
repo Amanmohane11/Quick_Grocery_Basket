52780f0975974a3dcbbf8e62eadda2472e4fda05
Amanmohane11 <164287485+Amanmohane11@users.noreply.github.com>
2025-06-09 06:44:45 -0400

    Update BestSeller.jsx

--- Client/client/src/components/BestSeller.jsx
+++ Client/client/src/components/BestSeller.jsx
@@ -7,10 +7,10 @@ const BestSeller = () => {
   const { products } = useAppContext(); // assuming products is part of context
 
   return (
-    <div className='mt-16'>
+    <div className='mt-16 ml-8.5'>
       <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
 
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6 ml-4'>
         {products
           .filter(product => product.inStock)
           .slice(0, 5)
